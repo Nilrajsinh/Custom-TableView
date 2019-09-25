eefe9665da04c68a93b9c968ac2068ef94f1e30f
nil <Nilrajsinh6005@ICLOUD.COM>
2019-09-25 16:56:42 +0530

    Initial Commit

---
+++ CustomCellNew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCellNew
+//
+//  Created by Nilrajsinh Vaghela on 25/09/19.
+//  Copyright © 2019 Cyphers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
